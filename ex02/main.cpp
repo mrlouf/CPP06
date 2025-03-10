fb652efcf169652c8d0d9b9705951c1bd891e384
nponchon <nponchon@student.42.fr>
2025-03-10 16:48:43 +0100

    and now?

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 16:03:22 by nponchon          #+#    #+#             */
-/*   Updated: 2025/03/10 16:26:03 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/03/10 16:45:42 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,28 @@
 
 int	main()
 {
-	srand(time(0));
-	Base *ptr = generate();
+	{
+		srand(time(0));
+		Base *ptr = generate();
 
-	std::cout << "Identify from pointer: ";
-	identify(ptr);
+		std::cout << "Identify from pointer: ";
+		identify(ptr);
 
-	std::cout << "Identify from reference: ";
-	identify(*ptr);
+		std::cout << "Identify from reference: ";
+		identify(*ptr);
 
-	delete ptr;
-	
+		delete ptr;
+	}
+	{
+		Base *ptr = NULL;
+
+		std::cout << "Identify from null pointer: ";
+		identify(ptr);
+
+		std::cout << "Identify from reference: ";
+		identify(*ptr);
+
+		delete ptr;
+	}
 	return (0);
 }
